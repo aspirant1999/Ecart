cba22c60c77d0d37f981f223876b1695f0856261
aspirant1999 <adarshpakkala1234@gmail.com>
2021-10-05 12:15:44 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Ecart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using products;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecart.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        productRepository repository = null;
+
+        //public HomeController()
+        //{
+        //    repository = new productRepository();
+        //}
+
+        [Route("/")]
+        public IActionResult GetallProducts()
+        {
+            var result = repository.getAllProducts();
+            if(result != null)
+                return View(result);
+            else
+                ViewBag
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ecart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
